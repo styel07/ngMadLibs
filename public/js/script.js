6862c2e4301b844bf700e85adf0ed36f6a99bffe
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-07-30 19:23:54 -0700

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+// Code goes here
+angular.module('myApp', [])
+.controller('myCtrl', ['$scope', function($scope) {
+ $scope.searchAll = function (choice) {
+    switch(choice) {
+      case 1:
+        $scope.femaleName = "";
+        break;
+      case 2:
+        $scope.dirtyTask = "";
+        break;
+      case 3:
+        $scope.obnoxiuousCelberity = "";
+        break;
+      case 4:
+        $scope.jobTitle = "";
+        break;
+      case 5:
+        $scope.celebrity = "";
+        break;
+      case 6:
+        $scope.hugeNumber = "";
+        break;
+      case 7:
+        $scope.tediousTask = "";
+        break;
+      case 8:
+        $scope.uselessTask = "";
+        break;
+      case 9:
+        $scope.adjective = "";
+        break;
+    }
+};
+
+$scope.init = function() {
+  $scope.femaleName='sarah';
+  $scope.dirtyTask='mud shoveling';
+  $scope.obnoxiuousCelberity='Miley Cyrus';
+  $scope.jobTitle = 'Programmer';
+  $scope.celebrity='Will Smith';
+  $scope.hugeNumber='100000';
+  $scope.tediousTask='folding clothes';
+  $scope.uselessSkill = 'juggle';
+  $scope.adjective = 'robust';
+};
+}]);
